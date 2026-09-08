395297bf9601076749db22c8d0ee4459a5a6e292
Amritha <ammuamritha746@gmail.com>
2026-09-08 23:05:43 +0530

    first commit

---
+++ test.py
@@ -0,0 +1,17 @@
+apple = 15.5
+orange = 20
+grape= 10.25
+total_volume_sold=apple + orange + grape
+print(total_volume_sold)
+total_volume = total_volume_sold
+final =int(total_volume)
+total = str(total_volume)
+
+print(total)
+print(type(total))
+print("total volume:", total)
+import random
+bonus = random.randrange(5,10)
+print(bonus)
+final_total = final + bonus 
+print(final_total)
