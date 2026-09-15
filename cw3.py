3a7539440e3677b697f2761399ad20c29409cb6c
Amritha <ammuamritha746@gmail.com>
2026-09-15 10:52:25 +0530

    add initial implementation of attendance tracking with full day count

---
+++ cw3.py
@@ -0,0 +1,10 @@
+has_account = True
+email_verified = False
+can_login = print( has_account and email_verified)
+email = "g@example.com"
+print( '@ 'in email)
+user_age = 17
+age =print("age:",user_age >= 18)
+email_verified = True
+
+print(can_login_final)
